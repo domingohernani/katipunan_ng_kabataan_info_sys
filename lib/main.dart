2cc60d3d7c00f6b90c4b6874ebe733a863c2ad7e
domingohernani <hernani.domingo1@gmail.com>
2024-07-06 03:06:35 +0800

    flutter project added

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:kk_information_system/screens/home.dart';
+
+void main() {
+  runApp(const Main());
+}
+
+class Main extends StatelessWidget {
+  const Main({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
---
+++ lib/responsive/login/layout_login.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LayoutLogin extends StatelessWidget {
+  const LayoutLogin({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
