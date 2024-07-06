c7a8b1be91ad8ca4eefaad5dd83e2ee9d4f9e8dd
domingohernani <hernani.domingo1@gmail.com>
2024-07-07 00:03:15 +0800

    notch and bottom system navigation color was change for better UI design

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:kk_information_system/screens/login.dart';
 
 void main() {
@@ -10,9 +11,22 @@ class Main extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Login(),
+      home: AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle(
+          statusBarColor: Colors.white, // Change this to the desired color
+          statusBarBrightness:
+              Brightness.dark, // For iOS (light text color for dark background)
+          statusBarIconBrightness: Brightness
+              .dark, // For Android (light text color for dark background)
+          systemNavigationBarColor:
+              Colors.white, // Change this to the desired color
+          systemNavigationBarIconBrightness:
+              Brightness.dark, // For Android (light icons for dark background)
+        ),
+        child: Login(),
+      ),
     );
   }
 }
