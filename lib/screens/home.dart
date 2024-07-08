cb6e0e8f5290d05908e4bcf6dd86e95f6d559734
domingohernani <hernani.domingo1@gmail.com>
2024-07-08 15:35:22 +0800

    changing tabbar view implemented

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:kk_information_system/services/auth_service.dart';
+import 'package:kk_information_system/sub-screens/account.dart';
+import 'package:kk_information_system/sub-screens/dashboard.dart';
+import 'package:kk_information_system/sub-screens/youth.dart';
 import 'package:provider/provider.dart';
 
 class Home extends StatefulWidget {
@@ -19,20 +22,63 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     final authService = Provider.of<AuthService>(context);
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Home"),
-        actions: [
-          IconButton(
-            onPressed: logout,
-            icon: FaIcon(
-              FontAwesomeIcons.arrowRightFromBracket,
-              size: 16,
+    return DefaultTabController(
+      length: 3, // number of tabs
+      child: Scaffold(
+        appBar: AppBar(
+          title: SizedBox(
+            width: 70,
+            child: Image.asset("lib/assets/logo_transparent.png"),
+          ),
+          actions: [
+            IconButton(
+              onPressed: logout,
+              icon: FaIcon(
+                FontAwesomeIcons.arrowRightFromBracket,
+                size: 16,
+              ),
             ),
-          )
-        ],
+          ],
+          bottom: TabBar(
+            labelColor: Theme.of(context).primaryColor,
+            unselectedLabelColor: Colors.black87,
+            indicatorColor: Theme.of(context).primaryColor,
+            tabs: [
+              Tab(
+                child: FaIcon(
+                  FontAwesomeIcons.chartPie,
+                  size: 20,
+                ),
+              ),
+              Tab(
+                child: FaIcon(
+                  FontAwesomeIcons.peopleGroup,
+                  size: 20,
+                ),
+              ),
+              Tab(
+                child: FaIcon(
+                  FontAwesomeIcons.userLarge,
+                  size: 20,
+                ),
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            Center(
+              child: Dashboard(),
+            ),
+            Center(
+              child: Youth(),
+            ),
+            Center(
+              child: Account(),
+            ),
+          ],
+        ),
       ),
-      body: Center(child: Text('Welcome, ${authService.user?.email}')),
     );
   }
 }
---
+++ lib/sub-screens/youth.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Youth extends StatelessWidget {
+  const Youth({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("Youth"),
+    );
+  }
+}
